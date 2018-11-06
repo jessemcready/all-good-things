6ee69c5227ce95054d1b92a44a8034bcd33e6dcb
Jesse McReady <jessemcready@gmail.com>
2018-11-06 09:21:57 -0500

    comments now show when submitted

--- src/components/posts/PostShow.js
+++ src/components/posts/PostShow.js
@@ -1,27 +1,57 @@
 import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { Container, Feed, Card, Icon } from 'semantic-ui-react'
+import { Feed, Card, Icon } from 'semantic-ui-react'
 import { postsUrl } from '../../constants/fetchUrls'
-import Post from './Post'
 import CommentContainer from '../../containers/CommentContainer'
 import Moment from 'react-moment'
+import { commentsUrl } from '../../constants/fetchUrls'
+import { createPostComment } from '../../actions/posts'
 
 class PostShow extends Component {
   state = {
     post: {},
+    comments: [],
     liked: false
   }
 
   componentDidMount(){
     const { id } = this.props.match.params
     fetch(`${postsUrl}/${id}`).then(res => res.json()).then( post => {
-      this.setState({ post })
+      this.setState({ post, comments: post.comments })
+    })
+  }
+
+  handleSubmit = (event, input) => {
+    const { user } = this.props
+    const { comments, post } = this.state
+    const { userInput } = input
+    fetch(commentsUrl, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify({
+        comment: {
+          post_id: post.id,
+          user_id: user.id,
+          content: userInput
+        }
+      })
+    }).then(res => res.json()).then( commentObj => {
+      this.setState({ comments: [commentObj, ...comments] })
+      createPostComment({
+        id: commentObj.id,
+        post_id: commentObj.post.id,
+        user: commentObj.user,
+        content: commentObj.content
+      })
     })
   }
 
   render() {
-    const { post : { comments, likes, user, created_at, content, liked } } = this.state
+    const { post : { user, created_at, content, liked } } = this.state
+    const { comments } = this.state
     const { id } = this.props.match.params
 
     if(user === undefined){
@@ -50,7 +80,7 @@ class PostShow extends Component {
               <Icon name='like' onClick={this.handleLike} />
             }
             </Feed.Like>
-            <CommentContainer comments={comments} postId={id} />
+            <CommentContainer comments={comments} postId={id} handleSubmit={this.handleSubmit} />
           </Feed.Meta>
         </Feed.Content>
         </Card>
@@ -62,7 +92,8 @@ class PostShow extends Component {
 
 const mapStateToProps = state => {
   return {
-    user: state.users
+    user: state.users,
+    posts: state.posts
   }
 }
 
--- src/containers/CommentContainer.js
+++ src/containers/CommentContainer.js
@@ -14,42 +14,21 @@ class CommentContainer extends Component {
     this.setState({ userInput: event.target.value })
   }
 
-  handleSubmit = (event, input) => {
-    const { user, postId } = this.props
-    const { userInput } = input
-    fetch(commentsUrl, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type':'application/json'
-      },
-      body: JSON.stringify({
-        comment: {
-          post_id: postId,
-          user_id: user.id,
-          content: userInput
-        }
-      })
-    }).then(res => res.json()).then( commentObj => {
-      this.setState({ userInput : '' })
-      createPostComment({
-        id: commentObj.id,
-        post_id: commentObj.post.id,
-        user: commentObj.user,
-        content: commentObj.content
-      })
-    })
+  handleComment = (event, input) => {
+    const { handleSubmit } = this.props
+    this.setState({ userInput: '' })
+    handleSubmit(event, input)
   }
 
   render() {
-    const {comments} = this.props
+    const { comments } = this.props
     const { userInput } = this.state
     return(
       <Comment.Group>
         <Header as='h3' dividing>
           Comments
         </Header>
-        <Form onSubmit={event => this.handleSubmit(event, this.state)}>
+        <Form onSubmit={event => this.handleComment(event, this.state)}>
           <Form.Input placeholder="Comment" fluid onChange={this.handleChange} value={userInput} />
         </Form>
         { comments.map( comment => <PostComment key={comment.id} {...comment} />) }
