15a208d3034ed044c0e45fc68384d59b1ac8e8aa
Jesse McReady <jessemcready@gmail.com>
2018-11-06 13:55:37 -0500

    removed unused constants, moved modal to its own component

--- src/containers/FeedContainer.js
+++ src/containers/FeedContainer.js
@@ -5,37 +5,9 @@ import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import { createPost } from '../actions/posts'
 import FetchAdapter from '../adapters/FetchAdapter'
+import NewPostModal from '../components/modals/NewPostModal'
 
 class FeedContainer extends Component{
-  state = {
-    userInput: '',
-    modalOpen: false
-  }
-
-  handleChange = (event) => {
-    if(this.state.userInput.length < 400){
-      this.setState({
-        userInput: event.target.value
-      })
-    }
-  }
-
-  handleSubmit = (event) => {
-    const { user, createPost } = this.props
-    const { userInput } = this.state
-    const post = { user_id: user.id, content: userInput }
-    FetchAdapter.createPost(post).then( postObj => {
-      this.setState({
-        userInput: '',
-        modalOpen: false
-      })
-      createPost(postObj)
-    })
-  }
-
-  handleOpen = () => this.setState({ modalOpen: true })
-
-  handleClose = () => this.setState({ modalOpen: false })
 
   orderPosts = posts => {
     return posts.sort((a,b) => {
@@ -45,11 +17,11 @@ class FeedContainer extends Component{
 
   render() {
     const { posts, user } = this.props
-    const { userInput } = this.state
     let combinedPosts = {}
     Object.assign(combinedPosts, posts)
     Object.assign(combinedPosts, user.posts)
     combinedPosts = this.orderPosts(posts)
+    debugger
     if(posts.length === 0){
       return (
         <Container textAlign='center' style={{marginTop: '75px'}}>
@@ -57,49 +29,15 @@ class FeedContainer extends Component{
             No posts to display. Click Discover above and search for some users!
             Or try creating your first post by clicking on the '+' in the bottom right.
           </Header>
-          <Modal
-            open={this.state.modalOpen}
-            onClose={this.handleClose}
-            trigger={
-              <Icon name='plus circle' size='huge' style={{position: 'fixed', bottom: '0', right: '0'}} onClick={this.handleOpen} />
-            }>
-            <Modal.Header>Create Post</Modal.Header>
-            <Modal.Content>
-              <Form onSubmit={this.handleSubmit}>
-                <TextArea
-                placeholder='Put down some thoughts'
-                onChange={this.handleChange}
-                value={userInput}
-                />
-                <Button>Submit</Button>
-              </Form>
-            </Modal.Content>
-          </Modal>
+          <NewPostModal />
         </Container>
       )
     }
 
     return (
       <Feed size='large' textAlign='center' style={{marginTop: '75px'}}>
-        {combinedPosts.map( post => <Post key={post.id} {...post} />)}
-        <Modal
-          open={this.state.modalOpen}
-          onClose={this.handleClose}
-          trigger={
-            <Icon name='plus circle' size='huge' style={{position: 'fixed', bottom: '0', right: '0'}} onClick={this.handleOpen} />
-          }>
-          <Modal.Header>Create Post</Modal.Header>
-          <Modal.Content>
-            <Form onSubmit={this.handleSubmit}>
-              <TextArea
-              placeholder='Put down some thoughts'
-              onChange={this.handleChange}
-              value={userInput}
-              />
-              <Button>Submit</Button>
-            </Form>
-          </Modal.Content>
-        </Modal>
+        {combinedPosts.map( post => <Post key={post.id} {...post} username={post.user.name} />)}
+        <NewPostModal />
       </Feed>
     )
   }
